6e0aa9922f88502df5f993821daed02bfe2141a4
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-05-29 07:26:41 +0200

    Scanning directory and reading files

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(__dirname)
+var path = require('path')
+var readDirFiles = require('read-dir-files')
+var endsWith = require('ends-with')
+var fs = require('fs')
+
+var rootPath = path.join(__dirname, 'sample-root')
+
+var listener = readDirFiles.list(rootPath, function (e, allFiles) {
+    var files = allFiles.filter(file => endsWith(file, '.js'))
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i]
+        fs.readFile(file, 'utf8', function (err, content) {
+            console.log(content)
+        })
+    }
+})
