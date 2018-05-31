86d2b936c56c554becd06e37d5c61efbbcfd2a1a
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2018-05-31 22:02:15 +0200

    Basic parsing define clauses

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-var path = require('path')
-var readDirFiles = require('read-dir-files')
-var endsWith = require('ends-with')
-var fs = require('fs')
+const path = require('path')
+const readDirFiles = require('read-dir-files')
+const endsWith = require('ends-with')
+const fs = require('fs')
+const util = require('util')
+const readFileAsync = util.promisify(fs.readFile)
 
-var rootPath = path.join(__dirname, 'sample-root')
+const rootPath = path.join(__dirname, 'sample-root')
 
-var listener = readDirFiles.list(rootPath, function (e, allFiles) {
-    var files = allFiles.filter(file => endsWith(file, '.js'))
+function getMatches(matchResult) {
+    var matches = [];
 
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i]
-        fs.readFile(file, 'utf8', function (err, content) {
-            console.log(content)
+    if (!matchResult) {
+        return matches;
+    }
+
+    for (var i = 1; i < matchResult.length; i++) {
+        matches.push(matchResult[i])
+    }
+
+    return matches;
+}
+
+const getModulesFromFileContent = function (content) {
+    var regex = /\s*define\s*\(\s*\[\s*('[^']*')(?:\s*,\s*('[^']*'))*\s*\]/i
+    var matchResult = content.match(regex)
+
+    return getMatches(matchResult, regex)
+}
+
+let listener = readDirFiles.list(rootPath, function (e, allFiles) {
+    let files = allFiles.filter(file => endsWith(file, '.js'))
+
+    let modulePromises = files.map(file => {
+        return readFileAsync(file, 'utf8').then(content => {
+            return getModulesFromFileContent(content)
+        })
+    })
+
+    Promise.all(modulePromises).then(modulesFromFiles => {
+        let result = modulesFromFiles.reduce((prev, current, index, array) => prev.concat(current), [])
+        result.forEach(element => {
+            console.log(element)
         })
     }
+    )
 })
